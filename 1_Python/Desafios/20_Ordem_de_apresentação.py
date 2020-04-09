185a95c3e50e469101b3d7a16630f8f9bd1c0854
Guilherme Silva <36104272+guilhermebaos@users.noreply.github.com>
2020-04-09 12:22:20 +0100

    Ficheiro duplicado

--- 1_Python/Desafios/20_Ordem_de_apresentação.py
+++
@@ -1,7 +0,0 @@
-import random
-a1 = input('Escreve o nome de um aluno: ')
-a2 = input('Escreve o nome de outro aluno: ')
-a3 = input('Escreve o nome de ainda outro aluno: ')
-a4 = input('Escreve o nome de um último aluno: ')
-ordem = random.sample([a1, a2, a3, a4], k=4)
-print('Os alunos vão apresentar por esta ordem: {}'.format(ordem))
